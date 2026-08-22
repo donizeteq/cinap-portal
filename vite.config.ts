e9e029e3aa6b87d267dcfbd851f9d6accb4619b1
Donizete Queiroz <donizete@zaptech.com.br>
2026-08-22 06:52:59 +0000

    chore(build): define base path relative for SPA deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      output: { manualChunks: undefined },
+    },
+  },
   server: {
     allowedHosts: true,
   },
